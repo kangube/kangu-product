6e0ce6ae651c9661b064e92f582fe258e650e4e9
Maarten Van Loock <maartenphone@gmail.com>
2016-03-01 15:04:48 +0100

    Animation changes
    - Made small changes to the animations

--- resources/js/custom/wow-init.js
+++ resources/js/custom/wow-init.js
@@ -1,1 +1,10 @@
-new WOW().init();
+var wow = new WOW({
+	boxClass:     'wow',
+	animateClass: 'animated',
+	offset:       0,
+	mobile:       true,
+	live:         true,
+	callback:     function(box) {},
+	scrollContainer: null
+});
+wow.init();
