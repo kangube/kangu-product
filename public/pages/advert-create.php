a7268411c8d7a69600167e1e9b14d2b9b35d9c08
Maarten Van Loock <maartenphone@gmail.com>
2016-03-04 16:28:15 +0100

    Advert class restructure
    - Restructured the entire advert class with new variables to hold all passed values
    - Rewrote multiple queries to insert the data in the right tables
    - Added a function in the advert-create page to dynamically calculate the advert price based on the services that will be provided
    - Added a function in the advert-create page to split the home adress into a street adress and city

--- public/pages/advert-create.php
+++ public/pages/advert-create.php
@@ -10,22 +10,55 @@
 	$stmt->execute(array(":user_id"=>$user_id));
 	$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 
-	$a1 = new Advert();
+	$advert = new Advert();
 
-	if(!empty($_POST))
+	if(isset($_POST['advert-create-button']))
 	{
 		try 
 		{	
-			
-			$a1->ID = $_POST['ID'];
-			$a1->Name = $_POST['name'];
-			$a1->Info = $_POST['info'];
+			// Splitting the given home adress into an street-adress and a city
+			$given_home_adress = explode(",", $_POST['advert-home-adress']);
+			$advert_street_adress = $given_home_adress[0];
+			$advert_city = $given_home_adress[1];
 
-			$a1->Save();
+			// Processing the chosen transportation options
+			$chosen_transportation_options = implode(", ", $_POST['advert-transportation']);
 
-			$succes = "Thank you for creating an advert!";
+			// Calculating the advert price based on the chosen services
+			$prices = array(
+				'opvang-thuisomgeving' => 3,
+				'ophalen-schoolpoort' => 2,
+				'vervoer-thuis' => 2,
+				'vervoer-naschoolse-activiteiten' => 2,
+				'voorzien-maaltijd' => 1,
+				'hulp-huiswerktaken' => 2
+			);
 
+			$services = $_POST['advert-services'];
+			$advert_price = 0;
 
+			foreach($services as $key => $service) {
+				if (!isset($prices[$service])) {
+					continue;
+				}
+				$advert_price += $prices[$service];
+			}
+
+			// Passing data to the advert class for processing
+			$advert->UserId = $userRow['user_id'];
+			$advert->Description = $_POST['advert-description'];
+			$advert->Price = $advert_price;
+			$advert->NumberChildren = $_POST['advert-spots'];
+			$advert->School = 'Heilig-hartcollege';
+			$advert->MobileNumber = $_POST['advert-mobile-number'];
+			$advert->HomeNumber = $_POST['advert-home-number'];
+			$advert->Email = $_POST['advert-email'];
+			$advert->HomeAdress = $advert_street_adress;
+			$advert->HomeCity = $advert_city;
+			$advert->Transportation = $chosen_transportation_options;
+			$advert->Services = $_POST['advert-services'];
+
+			$advert->Save();
 		}
 		catch(Exception $e)
 		{
@@ -61,23 +94,23 @@
 							<h3 class="form-header">Over deze advertentie</h3>
 							<hr class="blue-horizontal-line"></hr>
 							<p class="form-subheader">Presenteer jouw advertentie op de best mogelijke manier aan de hand van een gepersonaliseerde beschrijving van jezelf en jouw motivatie.</p>
-							<textarea name="textarea" placeholder="Geef een korte beschrijving jezelf en waarom je deze advertentie aanmaakt" required></textarea>
-							<div class="form-error">De beschrijving is te lang.</div>
+							<textarea placeholder="Geef een korte beschrijving jezelf en waarom je deze advertentie aanmaakt" name="advert-description" required></textarea>
+							<div class="form-error">Het is verplicht om een beschrijving te geven aan je advertentie.</div>
 						</div>
 
 						<div class="form-number-children-container">
 							<h3 class="form-header">Beschikbare plaatsen</h3>
 							<hr class="blue-horizontal-line"></hr>
 							<p class="form-subheader">Selecteer het maximum aantal kinderen waarvoor je opvang wenst aan te bieden.</p>
-
 							<div class="number-children-radio-buttons">
-							<label class="badge radio-button"><input type="radio" name="number-children" value="1" required>1</label>
-							<label class="badge radio-button"><input type="radio" name="number-children" value="2" required>2</label>
-							<label class="badge radio-button"><input type="radio" name="number-children" value="3" required>3</label>
-							<label class="badge radio-button"><input type="radio" name="number-children" value="4" required>4</label>
-							<label class="badge radio-button"><input type="radio" name="number-children" value="5" required>5</label>
-							<label class="badge radio-button"><input type="radio" name="number-children" value="6" required>6</label>
+								<label class="badge radio-button"><input type="radio" name="advert-spots" value="1" required>1</label>
+								<label class="badge radio-button"><input type="radio" name="advert-spots" value="2">2</label>
+								<label class="badge radio-button"><input type="radio" name="advert-spots" value="3">3</label>
+								<label class="badge radio-button"><input type="radio" name="advert-spots" value="4">4</label>
+								<label class="badge radio-button"><input type="radio" name="advert-spots" value="5">5</label>
+								<label class="badge radio-button"><input type="radio" name="advert-spots" value="6">6</label>
 							</div>
+							<div class="form-error">Geef aan hoeveel kinderen u maximum wenst op te vangen.</div>
 						</div>
 
 						<div class="form-contact-information-container">
@@ -88,7 +121,7 @@
 							<div class="small-12 medium-6 large-6 columns form-icon-input-field">
 								<div class="form-icon-input-container">
 									<span class="form-icon" data-icon="z"></span>
-									<input class="form-input" type="tel" placeholder="jouw gsm-nummer">
+									<input class="form-input" type="tel" name="advert-mobile-number" placeholder="jouw gsm-nummer" required>
 								</div>
 								<div class="form-error">Dit is geen geldig gsm-nummer.</div>
 							</div>
@@ -96,15 +129,15 @@
 							<div class="small-12 medium-6 large-6 columns form-icon-input-field">
 								<div class="form-icon-input-container">
 									<span class="form-icon" data-icon="q"></span>
-									<input class="form-input" type="tel" placeholder="jouw huistelefoonnummer">
+									<input class="form-input" type="tel" name="advert-home-number" placeholder="jouw huistelefoonnummer" required>
 								</div>
-								<div class="form-error">Dit is geen telefoonnummer.</div>
+								<div class="form-error">Dit is geen geldig huistelefoonnummer.</div>
 							</div>
 
 							<div class="small-12 medium-6 large-6 columns form-icon-input-field">
 								<div class="form-icon-input-container">
 									<span class="form-icon" data-icon="x"></span>
-									<input class="form-input" type="email" placeholder="jouw e-mail adres">
+									<input class="form-input" type="email" name="advert-email" placeholder="jouw e-mail adres" required>
 								</div>
 								<div class="form-error">Dit is geen geldig e-mail adres.</div>
 							</div>
@@ -112,9 +145,9 @@
 							<div class="small-12 medium-6 large-6 columns form-icon-input-field">
 								<div class="form-icon-input-container">
 									<span class="form-icon" data-icon="v"></span>
-									<input class="form-input" type="text" placeholder="jouw adres en gemeente">
+									<input class="form-input" type="text" name="advert-home-adress" placeholder="jouw adres en gemeente" required>
 								</div>
-								<div class="form-error">voorbeel: bosstraat 2, Heist-op-den-Berg</div>
+								<div class="form-error">voorbeeld: bosstraat 2, Heist-op-den-Berg</div>
 							</div>
 						</div>
 
@@ -123,24 +156,25 @@
 							<hr class="blue-horizontal-line"></hr>
 							<p class="form-subheader">Geef aan op welke manier je de kinderen van en naar school voert. (Meerdere opties zijn mogelijk)</p>
 							<div class="checkbox transportation-checkbox">
-								<input type="checkbox" id="auto"/>
+								<input type="checkbox" name="advert-transportation[]" id="auto" value="auto">
   								<label for="auto"><span></span>Met de auto</label>
 							</div>
 
 							<div class="checkbox transportation-checkbox">
-								<input type="checkbox" id="openbaar-vervoer"/>
+								<input type="checkbox" name="advert-transportation[]" id="openbaar-vervoer" value="openbaar-vervoer">
   								<label for="openbaar-vervoer"><span></span>Openbaar verv.</label>
 							</div>
 
 							<div class="checkbox transportation-checkbox">
-								<input type="checkbox" id="fiets"/>
+								<input type="checkbox" name="advert-transportation[]" id="fiets" value="fiets">
   								<label for="fiets"><span></span>Met de fiets</label>
 							</div>
 
 							<div class="checkbox transportation-checkbox">
-								<input type="checkbox" id="wandelend"/>
+								<input type="checkbox" name="advert-transportation[]" id="wandelend" value="wandelend">
   								<label for="wandelend"><span></span>Te voet</label>
 							</div>
+							<div class="form-error">Geef aan op welke manier je de kinderen vervoert van en naar school.</div>
 						</div>
 
 						<div class="small-12 columns form-services-container">
@@ -150,36 +184,39 @@
 
 							<div class="small-6 medium-4 columns services-checkbox">
 								<div class="checkbox">
-									<input type="checkbox" id="opvang"/>
+									<input type="checkbox" name="advert-services[]" id="opvang" value="opvang-thuisomgeving" 
+										   checked>
 	  								<label for="opvang"><span></span>Opvang in een thuisomgeving</label>
 								</div>
 
 								<div class="checkbox">
-									<input type="checkbox" id="ophalen"/>
+									<input type="checkbox" name="advert-services[]" id="ophalen" value="ophalen-schoolpoort" 
+										   checked>
 	  								<label for="ophalen"><span></span>Ophalen aan de schoolpoort</label>
 								</div>
 							</div>
 
 							<div class="small-6 medium-4 columns services-checkbox">
 								<div class="checkbox">
-									<input type="checkbox" id="vervoer-thuis"/>
+									<input type="checkbox" name="advert-services[]" id="vervoer-thuis" value="vervoer-thuis">
 	  								<label for="vervoer-thuis"><span></span>Vervoer naar thuis na opvang</label>
 								</div>
 
 								<div class="checkbox">
-									<input type="checkbox" id="vervoer-naschoolse-activiteiten"/>
+									<input type="checkbox" name="advert-services[]" id="vervoer-naschoolse-activiteiten" 
+										   value="vervoer-naschoolse-activiteiten">
 	  								<label for="vervoer-naschoolse-activiteiten"><span></span>Vervoer naschoolse activiteiten</label>
 								</div>
 							</div>
 
 							<div class="small-12 medium-4 columns services-checkbox">
 								<div class="checkbox">
-									<input type="checkbox" id="maaltijd"/>
-	  								<label for="maaltijd"><span></span>Voorzien van een maaltijd</label>
+									<input type="checkbox" name="advert-services[]" id="voorzien-maaltijd" value="voorzien-maaltijd">
+	  								<label for="voorzien-maaltijd"><span></span>Voorzien van een maaltijd</label>
 								</div>
 
 								<div class="checkbox">
-									<input type="checkbox" id="hulp-huiswerktaken"/>
+									<input type="checkbox" name="advert-services[]" id="hulp-huiswerktaken" value="hulp-huiswerktaken">
 	  								<label for="hulp-huiswerktaken"><span></span>Hulp bij huiswerktaken</label>
 								</div>
 							</div>
@@ -187,7 +224,7 @@
 
 						<div class="small-12 columns">
 							<p>Door deze advertentie aan te maken ga je akkoord met onze <a href="#">termen en condities</a></p>
-							<input id="advert-create-button" type="submit" value="Advertentie aanmaken"/>
+							<input id="advert-create-button" type="submit" name="advert-create-button" value="Advertentie aanmaken"/>
 						</div>
 					</form>
 				</div>
--- public/php-assets/class.advert.php
+++ public/php-assets/class.advert.php
@@ -4,8 +4,18 @@ require_once('class.dbconfig.php');
 
 class advert
 {	
-	private $m_sID;
+	private $m_iUserId;
 	private $m_sDescription;
+	private $m_iPrice;
+	private $m_iNumberChildren;
+	private $m_sSchool;
+	private $m_iMobileNumber;
+	private $m_iHomeNumber;
+	private $m_sEmail;
+	private $m_sHomeAdress;
+	private $m_sHomeCity;
+	private $m_sTransportation;
+	private $m_sServices;
 
 	private $conn;
 	
@@ -18,12 +28,52 @@ class advert
     public function __set($p_sProperty, $p_sValue) {
 		switch ($p_sProperty) 
 		{
-			case 'ID':
-				$this->m_sID = $p_sValue;
+			case 'UserId':
+				$this->m_iUserId = $p_sValue;
 				break;
 
 			case 'Description':
 				$this->m_sDescription = $p_sValue;
+				break;
+
+			case 'Price':
+				$this->m_iPrice = $p_sValue;
+				break;
+
+			case 'NumberChildren':
+				$this->m_iNumberChildren = $p_sValue;
+				break;
+
+			case 'School':
+				$this->m_sSchool = $p_sValue;
+				break;
+
+			case 'MobileNumber':
+				$this->m_iMobileNumber = $p_sValue;
+				break;
+
+			case 'HomeNumber':
+				$this->m_iHomeNumber = $p_sValue;
+				break;
+
+			case 'Email':
+				$this->m_sEmail = $p_sValue;
+				break;
+
+			case 'HomeAdress':
+				$this->m_sHomeAdress = $p_sValue;
+				break;
+
+			case 'HomeCity':
+				$this->m_sHomeCity = $p_sValue;
+				break;
+
+			case 'Transportation':
+				$this->m_sTransportation = $p_sValue;
+				break;
+
+			case 'Services':
+				$this->m_sServices = $p_sValue;
 				break;
 		}
 	}
@@ -31,25 +81,84 @@ class advert
 	public function __get($p_sProperty) {
 		switch ($p_sProperty) 
 		{
-			case 'ID':
-				return $this->m_sID;
+			case 'UserId':
+				return $this->m_iUserId;
 				break;
 
 			case 'Description':
 				return $this->m_sDescription;
+				break;
+
+			case 'Price':
+				return $this->m_iPrice;
+				break;
+
+			case 'NumberChildren':
+				return $this->m_iNumberChildren;
+				break;
+
+			case 'School':
+				return $this->m_sSchool;
+				break;
+
+			case 'MobileNumber':
+				return $this->m_iMobileNumber;
+				break;
+
+			case 'HomeNumber':
+				return $this->m_iHomeNumber;
+				break;
+
+			case 'Email':
+				return $this->m_sEmail;
+				break;
+
+			case 'HomeAdress':
+				return $this->m_sHomeAdress;
+				break;
+
+			case 'HomeCity':
+				return $this->m_sHomeCity;
+				break;
+
+			case 'Transportation':
+				return $this->m_sTransportation;
+				break;
+
+			case 'Services':
+				return $this->m_sServices;
 				break;
 		}
 	}
 	
 	public function Save() {
 		$conn = Db::getInstance();
-		$statement = $conn->prepare("INSERT INTO tbl_advert
-			(advert_id, advert_description) 
-			VALUES 
-			(:ID, :Description)");
-		$statement->bindValue(':ID', $this->ID );
-		$statement->bindValue(':Description', $this->Description );
+		$advert_query = "INSERT INTO tbl_advert(fk_user_id, advert_description, advert_price, advert_spots, advert_school, advert_transport) VALUES ($this->UserId, $this->Description, $this->Price, $this->NumberChildren, $this->School, $this->Transportation);";
+		$advert_query .= "INSERT INTO tbl_user(user_mobile_number, user_home_number, user_adress, user_city) VALUES ($this->MobileNumber, $this->HomeNumber, $this->HomeAdress, $this->HomeCity) WHERE user_id = $this->UserId;";
+
+		/*$advert_query .= "INSERT INTO tbl_services(fk_advert_id, service_name) VALUES ";
+		$iterator = new ArrayIterator($this->Services);
+		$cachingiterator = new CachingIterator($iterator);
+
+		foreach ($cachingiterator as $value)
+	    {
+	        $advert_query .= "(".$cachingiterator->current().")";
+
+	        if($cachingiterator->hasNext())
+	        {
+	            $advert_query .= ", ";
+	        }
+	    }*/
+
+	   	//$statement = $conn->prepare($advert_query);
+		//$statement->execute();
+
+	    echo $advert_query;
+	    $statement = $conn->prepare(mysqli_multi_query($advert_query));
 		$statement->execute();
+
+	    //$last_created_id = mysqli_insert_id($conn);
+	    //echo "The last created advert-id is: ".$last_created_id;
 	}
 	
 	public function getAll() {
