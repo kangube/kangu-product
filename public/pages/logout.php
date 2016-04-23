20210902e57e2b3ac5b2bb22adf9f8d0e1182901
dirixtom <dirixtom@gmail.com>
2016-04-23 12:10:30 +0200

    Changes to logout and added redirect to advert-create

--- public/pages/logout.php
+++ public/pages/logout.php
@@ -4,6 +4,7 @@
 	
 	$user_logout = new USER();
 	
+	/*
 	if($user_logout->is_loggedin()!="")
 	{
 		$user_logout->redirect('login.php');
@@ -12,4 +13,12 @@
 	{
 		$user_logout->doLogout();
 		$user_logout->redirect('login.php');
-	}
+	}*/
+
+	// Voorwa al die moeite als alles wa ge moet doen is een sessie wegdoen?
+	// De eerste if werkt al ni omda ge zegt als de user ingelogd is, da ge af de pagina moet en ni moogt uitloggen
+	// De tweede if moet ten eerste al nen if else zijn en al die hassle moet er ni zijn want ge hebt maar 2 lijnen code nodig
+
+	session_destroy();
+	$user_logout->redirect('login.php');
+
