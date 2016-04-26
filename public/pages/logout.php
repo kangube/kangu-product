746c1e7d615e8e08d0eaecf12dd630ff94d2203c
dirixtom <dirixtom@gmail.com>
2016-04-26 14:16:29 +0200

    Cleaned logout.php
    Removed commentary code

--- public/pages/logout.php
+++ public/pages/logout.php
@@ -3,22 +3,6 @@
 	require_once('../php-assets/class.user.php');
 	
 	$user_logout = new USER();
-	
-	/*
-	if($user_logout->is_loggedin()!="")
-	{
-		$user_logout->redirect('login.php');
-	}
-	if(isset($_GET['logout']) && $_GET['logout']=="true")
-	{
-		$user_logout->doLogout();
-		$user_logout->redirect('login.php');
-	}*/
-
-	// Voorwa al die moeite als alles wa ge moet doen is een sessie wegdoen?
-	// De eerste if werkt al ni omda ge zegt als de user ingelogd is, da ge af de pagina moet en ni moogt uitloggen
-	// De tweede if moet ten eerste al nen if else zijn en al die hassle moet er ni zijn want ge hebt maar 2 lijnen code nodig
 
 	session_destroy();
 	$user_logout->redirect('login.php');
-
