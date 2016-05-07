02d0d59ce126fe254961548e23be38be7098d298
dirixtom <dirixtom@gmail.com>
2016-05-07 07:29:49 +0200

    Modified queries and added visibility toggle

--- public/pages/planning-overview.php
+++ public/pages/planning-overview.php
@@ -17,103 +17,89 @@
         <title>Advertentie-overzicht</title>
         <link rel="stylesheet" href="../css/minimum-viable-product.min.css">
         <link href="https://file.myfontastic.com/QxAJVhmfbQ2t7NGCUAnz9P/icons.css" rel="stylesheet">
+        <style>
+        .hidden {
+        	display: none;
+        }
+        .visible {
+        	display: block;
+        }
+        </style>
     </head>
 
 	<body>	
 		<?php include('../php-includes/navigation.php'); ?>
-	
-		<div class="large-12 columns datepicker-small">
-		    	<h2 class="mrgtop">Boekingen</h2>
-		    	<br/>
-		    	<h3>Advertenties die ik geboekt heb</h3>
-		    	<?php
+		<ul id="toggle">
+			<li class="to-boeked"><a href="#">boeken</a></li>
+    		<li class="to-boeken"><a href="#">boeked</a></li>
+    	</ul>
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    	<script>
+	    	$(document).ready(function () {
+			    $('.to-boeked').on('click', function () {
+			    	//Maak boeked visible
+			    	$('.boeked').removeClass('hidden');
+			        $('.boeked').addClass('visible');
+			        //Maak boeken invisible
+			        $('.boeken').removeClass('visible');
+			        $('.boeken').addClass('hidden');
+			    });
+			    $('.to-boeken').on('click', function () {
+			    	//Maak boeken visible
+			    	$('.boeken').removeClass('hidden');
+			        $('.boeken').addClass('visible');
+			        //Maak boeked invisible
+			        $('.boeked').removeClass('visible');
+			        $('.boeked').addClass('hidden');
+			    });
+			});
+    	</script>
+		<div class="boeken visible">
+			<div class="large-12 columns datepicker-small">
+			    	<h3>Datums waarop ik anderen geboekt heb</h3>
+					<?php
+						//Datums tonen waarop ik anderen geboekt heb
+						$booking_results = $mysqli->prepare("SELECT fk_booking_id, booking_date_format, tbl_booking.fk_booker_user_id, tbl_booking.fk_renter_user_id FROM tbl_booking_dates LEFT JOIN tbl_booking ON tbl_booking_dates.fk_booking_id=tbl_booking.booking_id WHERE tbl_booking.fk_renter_user_id = ".$userRow['user_id']."");
+						$booking_results->execute();
+						$booking_results->bind_result($fk_booking_id, $booking_date_format, $fk_booker_user_id, $fk_renter_user_id);
 
-					$advert_results = $mysqli->prepare("SELECT COUNT(tbl_booking.booking_id), tbl_booking.booking_id, tbl_booking.fk_advert_id, tbl_booking.fk_booker_user_id, tbl_booking.fk_renter_user_id, tbl_booking.booking_number_spots, tbl_booking.booking_price, tbl_booking.booking_extra_information FROM tbl_booking WHERE tbl_booking.fk_renter_user_id = ".$userRow['user_id']."");
-					$advert_results->execute();
-					$advert_results->bind_result($COUNT, $booking_id, $fk_advert_id, $fk_booker_user_id, $fk_renter_user_id, $booking_number_spots, $booking_price, $booking_extra_information);
-
-					while($advert_results->fetch()) 
-					{
-						echo "Advertentie geboekt: #".$fk_advert_id."<br/>";
-					}
-
-				?>
-				<br/>
-		    	<h3>Mijn advertentie is geboekt door</h3>
-		    	<?php
-					$advert_results = $mysqli->prepare("SELECT tbl_booking.booking_id, tbl_booking.fk_advert_id, tbl_booking.fk_booker_user_id, tbl_booking.fk_renter_user_id, tbl_booking.booking_number_spots, tbl_booking.booking_price, tbl_booking.booking_extra_information FROM tbl_booking WHERE tbl_booking.fk_booker_user_id = ".$userRow['user_id']."");
-					$advert_results->execute();
-					$advert_results->bind_result($booking_id, $fk_advert_id, $fk_booker_user_id, $fk_renter_user_id, $booking_number_spots, $booking_price, $booking_extra_information);
-
-					while($advert_results->fetch()) 
-					{
-						echo "Mijn advertentie is geboekt door: User ".$fk_renter_user_id."<br/>";
-					}
-				?>
-		</div>
-		
-		<div class="large-12 columns datepicker-small">
-		    	<h2 class="mrgtop">Datums van boekingen</h2>
-		    	<br/>
-		    	<h3>Datums waarop ik anderen geboekt heb</h3>
-				<?php
-					//Datums formatteren om te tonen in agenda, waarop ik anderen geboekt heb
-					$bookingdate_results = $mysqli->prepare("SELECT fk_booking_id, booking_date_format FROM tbl_booking_dates LEFT JOIN tbl_booking ON tbl_booking_dates.fk_booking_id=tbl_booking.booking_id WHERE tbl_booking.fk_renter_user_id = ".$userRow['user_id']."");
-					$bookingdate_results->execute();
-					$bookingdate_results->bind_result($fk_booking_id, $booking_date_format);
-
-						while($bookingdate_results->fetch()) 
-						{	
-							//echo "Datums die bij advertentie horen: ".$availability_date;
-							$originalDate = $booking_date_format;
-							//$sign = ', ';
-							$newDateBooking = date("m/d/Y", strtotime($originalDate));	
-							//echo "<br/>Geformatteerde datum: ".$newDate;
-							//echo $newDateBooking;
+						while($booking_results->fetch()) 
+						{
+							echo "Datum: ".$booking_date_format."<br/>";
+							echo "Gehuurde: #".$fk_booker_user_id."<br/><br/>";
 						}
-
-					//Datums tonen waarop ik anderen geboekt heb
-					$booking_results = $mysqli->prepare("SELECT fk_booking_id, booking_date_format FROM tbl_booking_dates LEFT JOIN tbl_booking ON tbl_booking_dates.fk_booking_id=tbl_booking.booking_id WHERE tbl_booking.fk_renter_user_id = ".$userRow['user_id']."");
-					$booking_results->execute();
-					$booking_results->bind_result($fk_booking_id, $booking_date_format);
-
-					while($booking_results->fetch()) 
-					{
-						echo "Datum: ".$booking_date_format."<br/>";
-					}
-				?>
-				<br/>
-		    	<h3>Datums waarop ik geboekt ben</h3>
-				<?php
-					//Datums formatteren om te tonen in agenda, waarop ik geboekt ben
-					$bookingdate_results = $mysqli->prepare("SELECT COUNT(fk_booking_id), fk_booking_id, booking_date_format FROM tbl_booking_dates LEFT JOIN tbl_booking ON tbl_booking_dates.fk_booking_id=tbl_booking.booking_id WHERE tbl_booking.fk_booker_user_id = ".$userRow['user_id']."");
-					$bookingdate_results->execute();
-					$bookingdate_results->bind_result($count, $fk_booking_id, $booking_date_format);
-						
-							while($bookingdate_results->fetch()) 
-							{
-								$originalDate = $booking_date_format;
-								$newDateBooked = date("m/d/Y", strtotime($originalDate));
-							}
-						
-					//Datums tonen waarop ik geboekt ben
-					$booked_results = $mysqli->prepare("SELECT fk_booking_id, booking_date_format FROM tbl_booking_dates LEFT JOIN tbl_booking ON tbl_booking_dates.fk_booking_id=tbl_booking.booking_id WHERE tbl_booking.fk_booker_user_id = ".$userRow['user_id']."");
-					$booked_results->execute();
-					$booked_results->bind_result($fk_booking_id, $booking_date_format);
-
-					while($booked_results->fetch()) 
-					{
-						echo "Datum: ".$booking_date_format."<br/>";
-					}
-				?>
-		</div>
-
-	    <div class="large-6 columns datepicker-small">
+					?>
+			</div>
+			<!--<div class="large-6 columns datepicker-small">
 		    	<h2 class="mrgtop">Dagen waarop ik geboekt ben</h2>
 		    	<hr class="blue-horizontal-line"></hr>
 
 				<div id="planning-events"></div>
+			</div>-->
 		</div>
+		<div class="boeked hidden">
+			<div class="large-12 columns datepicker-small">
+		    	<h3>Datums waarop ik geboekt ben</h3>
+				<?php
+					//Datums tonen waarop ik geboekt ben
+					$booking_results = $mysqli->prepare("SELECT fk_booking_id, booking_date_format, tbl_booking.fk_booker_user_id, tbl_booking.fk_renter_user_id FROM tbl_booking_dates LEFT JOIN tbl_booking ON tbl_booking_dates.fk_booking_id=tbl_booking.booking_id WHERE tbl_booking.fk_booker_user_id = ".$userRow['user_id']."");
+					$booking_results->execute();
+					$booking_results->bind_result($fk_booking_id, $booking_date_format, $fk_booker_user_id, $fk_renter_user_id);
+
+					while($booking_results->fetch()) 
+					{
+						echo "Datum: ".$booking_date_format."<br/>";
+						echo "Huurder: #".$fk_renter_user_id."<br/><br/>";
+					}
+				?>
+		</div>
+		<!--<div class="large-6 columns datepicker-small">
+	    	<h2 class="mrgtop">Dagen waarop ik geboekt ben</h2>
+	    	<hr class="blue-horizontal-line"></hr>
+
+			<div id="planning-events2"></div>
+		</div>-->
+	</div>
 
 </body>
 </html>
