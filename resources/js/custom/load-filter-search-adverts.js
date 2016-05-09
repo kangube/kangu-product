12d4416237b2da6e365c762a8f687e5f27fe717b
Maarten Van Loock <maartenphone@gmail.com>
2016-05-09 12:13:27 +0200

    Restructure search-function
    - Restructured the queries of the search-function
    - Added a date-field to the search function
    - Search results are now displaying the number of available spots for an advert with a certain date
    - Restructured the filters for the search function
    - Removed the search page as it wasn't being used anymore
    - Resolved the conflicts that were caused in the mobile search function

--- resources/js/custom/load-filter-search-adverts.js
+++ resources/js/custom/load-filter-search-adverts.js
@@ -1,17 +1,42 @@
 $(document).ready(function() {
 	var filter = "";
+	var school = "";
+	var date = "";
+	var price = "";
+	var spots = "";
 
-	$(".advert-search-form, .advert-search-form-mobile").on("submit", function (e) {
+	$(".advert-search-form").on("submit", function (e) {
 		e.preventDefault();
-		var school = $('.search-region').val();
-		var price = $('.search-price').val();
-		var spots = $('.search-spots').val();
+		school = $('.search-region').val();
+		date = $('.search-date').val();
+		price = $('.search-price').val();
+		spots = $('.search-spots').val();
 
 		$.ajax({
 			type: 'post',
 			dataType: 'html',
 			url: '../php-assets/class.search.php',
-			data: {school:school, price:price, spots:spots},
+			data: {school:school, date:date, price:price, spots:spots},
+			cache: false,
+			success: function(response) {
+				$(".advert-overview-container").css("display", "none");
+				$(".search-advert-overview-container" ).html(response);
+			}
+		});
+	});
+
+	$(".advert-search-form-mobile").on("submit", function (e) {
+		e.preventDefault();
+		school = $('.search-region-mobile').val();
+		date = $('.search-date-mobile-alt').val();
+		price = $('.search-price-mobile').val();
+		spots = $('.search-spots-mobile').val();
+
+		$.ajax({
+			type: 'post',
+			dataType: 'html',
+			url: '../php-assets/class.search.php',
+			data: {school:school, date:date, price:price, spots:spots},
 			cache: false,
 			success: function(response) {
 				$(".advert-overview-container").css("display", "none");
@@ -22,15 +47,12 @@ $(document).ready(function() {
 
 	$(".search-advert-overview-container").on("change", ".search-advert-overview-filter", function(e) {
 		filter = $(this).val();
-		var school = $('.search-region').val();
-		var price = $('.search-price').val();
-		var spots = $('.search-spots').val();
 
 		$.ajax({
 			type: 'post',
 			dataType: 'html',
 			url: '../php-assets/class.search.php',
-			data: {chosenFilter:filter, filterSchool:school, filterPrice:price, filterSpots:spots},
+			data: {chosenFilter:filter, filterSchool:school, filterDate:date, filterPrice:price, filterSpots:spots},
 			cache: false,
 			success: function(response) {
 				$(".advert-overview-container").css("display", "none");
@@ -42,17 +64,13 @@ $(document).ready(function() {
 
 	$(".search-advert-overview-container").on("click", ".pagination a", function (e) {
 		e.preventDefault();
-		var school = $('.search-region').val();
-		var price = $('.search-price').val();
-		var spots = $('.search-spots').val();
 		var page = $(this).attr("data-page");
 
 		if (!filter) {
-			$(".search-advert-overview-container").load("../php-assets/class.search.php", {page:page, school:school, price:price, spots:spots});
+			$(".search-advert-overview-container").load("../php-assets/class.search.php", {page:page, school:school, date:date, price:price, spots:spots});
 		}
 		else if (filter) {
-    		//alert("chosen filter: "+filter+", school: "+school+", price: "+price+", spots: "+spots+".");
-			$(".search-advert-overview-container").load("../php-assets/class.search.php", {page:page, chosenFilter:filter, filterSchool:school, filterPrice:price, filterSpots:spots});
+			$(".search-advert-overview-container").load("../php-assets/class.search.php", {page:page, chosenFilter:filter, filterSchool:school, filterDate:date, filterPrice:price, filterSpots:spots});
 		}
 	});
 });
